b784ba088fb36602f921e3ac77d2405df7b36fb3
Douglas Faria <hfsdouglas@gmail.com>
2025-03-04 18:06:28 -0300

    feat: add create classrooms table migration

---
+++ database/migrations/2025_03_04_205920_create_classrooms_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('classroom');
+            $table->timestamp('created_at')->default(now());
+            $table->timestamp('updated_at')->default(now());
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classrooms');
+    }
+};
