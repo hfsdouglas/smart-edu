8362c19617122706060b60d6a9351800918e51ed
Douglas Faria <hfsdouglas@gmail.com>
2025-03-04 18:06:57 -0300

    feat: add create classes table migration

---
+++ database/migrations/2025_03_04_210213_create_classes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreign('subject_id')->references('id')->on('subjects');
+            $table->foreign('classroom_id')->references('id')->on('classrooms');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->timestamp('created_at')->default(now());
+            $table->timestamp('updated_at')->default(now());
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classes');
+    }
+};
