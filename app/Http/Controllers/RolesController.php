ea253a31f38476762d9a297f93745ae4230efd2f
Douglas Faria <hfsdouglas@gmail.com>
2025-03-04 22:11:04 -0300

    feat: create controler and service to insert a role at the db

---
+++ app/Http/Controllers/RolesController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Roles;
+use App\Models\Schools;
+use Illuminate\Http\Request;
+
+class RolesController
+{
+    public function index() {
+        $roles = Roles::all();
+        
+        return response()->json([
+            'roles' => $roles
+        ]);
+    }
+    
+    public function create(Request $request) {
+        $role = $request->input('role');
+        $school_id = $request->input('school_id');
+
+        if (!$school_id) {
+            return response()->json([
+                'message' => 'O ID da escola não foi encontrado!'
+            ], 400);
+        }
+
+        if (!$role) {
+            return response()->json([
+                'message' => 'Nome do cargo vazio!'
+            ], 400);
+        }
+
+        $school = Schools::find($school_id);
+
+        if (!$school) {
+            return response()->json([
+                'message' => 'Escola não encontrada!'
+            ], 404);
+        }
+
+        $role = Roles::create([
+            'role' => $role,
+            'school_id' => $school_id
+        ]);
+
+        return response()->json([
+            'message' => 'Cargo cadastrado com sucesso!',
+            'role' => $role
+        ]);
+    }
+}
