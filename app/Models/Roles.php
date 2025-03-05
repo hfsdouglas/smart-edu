22727f364a5295a66b73163e65471995eb3756a7
Douglas Faria <hfsdouglas@gmail.com>
2025-03-04 22:54:27 -0300

    fix:  function name to schools

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -10,11 +11,12 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 class Roles extends Model
 {
     use HasUuids;
+    use HasFactory;
 
     protected $table = 'roles';
     protected $fillable = ['role', 'school_id'];
 
-    public function school(): BelongsTo {
-        return $this->belongsTo(Schools::class);
+    public function schools(): BelongsTo {
+        return $this->belongsTo(Schools::class, 'school_id');
     }
 }
