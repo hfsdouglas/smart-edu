05dd40f9df486142d7f2bd6ff3b149dd9f2d6f67
Douglas Faria <hfsdouglas@gmail.com>
2025-03-04 22:11:57 -0300

    feat: create a post route to /roles for creating new roles

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\RolesController;
 use App\Http\Controllers\SchoolsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/schools', [SchoolsController::class, 'index']);
 Route::get('/schools/{id}', [SchoolsController::class, 'show']);
 Route::post('/schools', [SchoolsController::class, 'create']);
+
+Route::post('/roles', [RolesController::class, 'create']);
